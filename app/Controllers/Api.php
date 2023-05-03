dd3c01635959d1ab22b2a36b5b4a1e544a2c3248
Mark Glen Sadang Miguel <markgmiguel@outlook.com>
2023-05-03 09:53:35 +0800

    Added Products CRUD API

--- app/Controllers/Api.php
+++ app/Controllers/Api.php
@@ -3,11 +3,13 @@
 namespace App\Controllers;
 
 use App\Models\Student_model;
+use App\Models\Product_model;
 
 class Api extends BaseController
 {
     public function __construct() {
         $this->StudentModel = new Student_model();
+        $this->ProductModel = new Product_model();
     }
 
     public function student(){
@@ -267,6 +269,308 @@ class Api extends BaseController
                             $respData = [
                                 'meta' => array('code' => 200,
                                                 'message' => 'Student Record Deleted Successfully!',),
+                                'data' => '',
+                            ];
+                        } else {
+                            $respData = [
+                                'meta' => array('code' => 500,
+                                                'message' => $result,),
+                                'data' => '',
+                            ];
+                        }
+
+                    } catch(\Exception $e){
+                        die($e->getMessage());
+                    }
+                } else {
+                    $respData = [
+                        'meta' => array('code' => 301,
+                                        'message' => 'Bad request. Student No, First Name, Middle Name, Last Name, Sex, and Date of Birth is Required.',),
+                    ];
+                }
+            } else {
+                $respData = [
+                    'meta' => array('code' => 301,
+                                    'message' => 'Bad request. DATA is Required',),
+                ];
+            }
+        }
+
+        return $this->response->setJSON($respData);
+    }
+
+    public function products(){
+        $method =  $this->request->getMethod();
+        
+        if ($method == 'get'){
+            $products_info = $this->ProductModel->getProductInfo();
+
+            $respData = [
+                'meta' => array('code' => 200,
+                                'message' => 'Get Product Record',),
+                'data' => array('products_info' => $products_info,),
+            ];
+
+        } else if ($method == 'post'){
+            $postData = $this->request->getPost();
+
+            if($postData){
+                if(
+                    isset($postData['product_category']) && isset($postData['product_name']) &&
+                    isset($postData['product_description']) && isset($postData['product_price']) &&
+                    isset($postData['product_quantity']) && isset($postData['product_slug'])
+                ){
+                    try{
+                        $product_category = $postData['product_category'];
+                        $product_name = $postData['product_name'];
+                        $product_description = $postData['product_description'];
+                        $product_price = $postData['product_price'];
+                        $product_quantity = $postData['product_quantity'];
+                        $product_slug = $postData['product_slug'];
+
+                        if (!$product_category){
+                            $respData = [
+                                'meta' => array('code' => 412,
+                                                'message' => 'Product Category is required!',),
+                                'data' => '',
+                            ];
+                            return $this->response->setJSON($respData);
+                        }
+
+                        if (!$product_name){
+                            $respData = [
+                                'meta' => array('code' => 412,
+                                                'message' => 'Product Name is required!',),
+                                'data' => '',
+                            ];
+                            return $this->response->setJSON($respData);
+                        }
+
+                        if (!$product_description){
+                            $respData = [
+                                'meta' => array('code' => 412,
+                                                'message' => 'Product Description is required!',),
+                                'data' => '',
+                            ];
+                            return $this->response->setJSON($respData);
+                        }
+
+                        if (!$product_price){
+                            $respData = [
+                                'meta' => array('code' => 412,
+                                                'message' => 'Product Price is required!',),
+                                'data' => '',
+                            ];
+                            return $this->response->setJSON($respData);
+                        }
+
+                        if (!$product_quantity){
+                            $respData = [
+                                'meta' => array('code' => 412,
+                                                'message' => 'Product Quantity is required!',),
+                                'data' => '',
+                            ];
+                            return $this->response->setJSON($respData);
+                        }
+
+                        if (!$product_slug){
+                            $respData = [
+                                'meta' => array('code' => 412,
+                                                'message' => 'Product Slug is required!',),
+                                'data' => '',
+                            ];
+                            return $this->response->setJSON($respData);
+                        }
+
+                        $postdata = array(
+                            "product_category" => $product_category,
+                            "product_name" => $product_name,
+                            "product_description" => $product_description,
+                            "product_price" => $product_price,
+                            "product_quantity" => $product_quantity,
+                            "product_slug" => $product_slug,
+                        );
+
+                        $result = $this->ProductModel->insertProduct($postdata);
+                        if($result == 1){
+                            $respData = [
+                                'meta' => array('code' => 200,
+                                                'message' => 'Product Record Inserted Successfully!',),
+                                'data' => '',
+                            ];
+                        } else {
+                            $respData = [
+                                'meta' => array('code' => 500,
+                                                'message' => $result,),
+                                'data' => '',
+                            ];
+                        }
+
+                    }catch(\Exception $e){
+                        die($e->getMessage());
+                    }
+                } else {
+                    $respData = [
+                        'meta' => array('code' => 301,
+                                        'message' => 'Bad request. Product Category, Name, Description, Price, Quantity and Slug is Required.',),
+                    ];
+                }
+            } else {
+                $respData = [
+                    'meta' => array('code' => 301,
+                                    'message' => 'Bad request. DATA is Required',),
+                ];
+            }
+
+        } else if ($method == 'put'){
+            $postData = $this->request->getRawInput();
+
+            if($postData){
+                if(
+                    isset($postData['product_id']) &&
+                    isset($postData['product_category']) && isset($postData['product_name']) &&
+                    isset($postData['product_description']) && isset($postData['product_price']) &&
+                    isset($postData['product_quantity']) && isset($postData['product_slug'])
+                ){
+                    try{
+                        $product_id = $postData['product_id'];
+                        $product_category = $postData['product_category'];
+                        $product_name = $postData['product_name'];
+                        $product_description = $postData['product_description'];
+                        $product_price = $postData['product_price'];
+                        $product_quantity = $postData['product_quantity'];
+                        $product_slug = $postData['product_slug'];
+
+                        if (!$product_id){
+                            $respData = [
+                                'meta' => array('code' => 412,
+                                                'message' => 'Product ID is required!',),
+                                'data' => '',
+                            ];
+                            return $this->response->setJSON($respData);
+                        }
+
+                        if (!$product_category){
+                            $respData = [
+                                'meta' => array('code' => 412,
+                                                'message' => 'Product Category is required!',),
+                                'data' => '',
+                            ];
+                            return $this->response->setJSON($respData);
+                        }
+
+                        if (!$product_name){
+                            $respData = [
+                                'meta' => array('code' => 412,
+                                                'message' => 'Product Name is required!',),
+                                'data' => '',
+                            ];
+                            return $this->response->setJSON($respData);
+                        }
+
+                        if (!$product_description){
+                            $respData = [
+                                'meta' => array('code' => 412,
+                                                'message' => 'Product Description is required!',),
+                                'data' => '',
+                            ];
+                            return $this->response->setJSON($respData);
+                        }
+
+                        if (!$product_price){
+                            $respData = [
+                                'meta' => array('code' => 412,
+                                                'message' => 'Product Price is required!',),
+                                'data' => '',
+                            ];
+                            return $this->response->setJSON($respData);
+                        }
+
+                        if (!$product_quantity){
+                            $respData = [
+                                'meta' => array('code' => 412,
+                                                'message' => 'Product Quantity is required!',),
+                                'data' => '',
+                            ];
+                            return $this->response->setJSON($respData);
+                        }
+
+                        if (!$product_slug){
+                            $respData = [
+                                'meta' => array('code' => 412,
+                                                'message' => 'Product Slug is required!',),
+                                'data' => '',
+                            ];
+                            return $this->response->setJSON($respData);
+                        }
+
+                        $postdata = array(
+                            "product_id" => $product_id,
+                            "product_category" => $product_category,
+                            "product_name" => $product_name,
+                            "product_description" => $product_description,
+                            "product_price" => $product_price,
+                            "product_quantity" => $product_quantity,
+                            "product_slug" => $product_slug,
+                        );
+
+                        $result = $this->ProductModel->updateProduct($postdata, $product_id);
+
+                        if($result == 1){
+                            $respData = [
+                                'meta' => array('code' => 200,
+                                                'message' => 'Product Record Updated Successfully!',),
+                                'data' => '',
+                            ];
+                        } else {
+                            $respData = [
+                                'meta' => array('code' => 500,
+                                                'message' => $result,),
+                                'data' => '',
+                            ];
+                        }
+
+                    }catch(\Exception $e){
+                        die($e->getMessage());
+                    }
+                } else {
+                    $respData = [
+                        'meta' => array('code' => 301,
+                                        'message' => 'Bad request. Product Category, Name, Description, Price, Quantity and Slug is Required.',),
+                    ];
+                }
+            } else {
+                $respData = [
+                    'meta' => array('code' => 301,
+                                    'message' => 'Bad request. DATA is Required',),
+                ];
+            }
+
+        } else if ($method == 'delete'){
+            $postData = $this->request->getRawInput();
+
+            if($postData){
+                if(
+                    isset($postData['product_id'])
+                ){
+                    try{
+                        $product_id = $postData['product_id'];
+
+                        if (!$product_id){
+                            $respData = [
+                                'meta' => array('code' => 412,
+                                                'message' => 'Product ID is required!',),
+                                'data' => '',
+                            ];
+                            return $this->response->setJSON($respData);
+                        }
+
+                        $result = $this->ProductModel->deleteProduct($product_id);
+                        
+                        if($result == 1){
+                            $respData = [
+                                'meta' => array('code' => 200,
+                                                'message' => 'Product Record Deleted Successfully!',),
                                 'data' => '',
                             ];
                         } else {
